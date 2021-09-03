754d33f4d8f19923c874f4bdbf65297417d80756
rhythm244 <63957603+rhythm244@users.noreply.github.com>
2021-09-03 14:28:48 +0700

    sending props to component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-
-  const expenseDate = new Date(2021, 2, 28).toLocaleDateString();
-  const expenseTitle = 'Car Insurance'
-  const expenseAmout = 294.67
+function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <div>{expenseDate}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmout}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
