496f3219155c23aa6237ed684d9b290a8c0a660c
rhythm244 <63957603+rhythm244@users.noreply.github.com>
2021-09-03 14:13:48 +0700

    dynamic placeholder

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,17 @@
-import classes from "./ExpenseItem.css";
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
 
-
+  const expenseDate = new Date(2021, 2, 28).toLocaleDateString();
+  const expenseTitle = 'Car Insurance'
+  const expenseAmout = 294.67
 
   return (
     <div className="expense-item">
-      <div>March 28.</div>
+      <div>{expenseDate}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">249.79</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmout}</div>
       </div>
     </div>
   );
