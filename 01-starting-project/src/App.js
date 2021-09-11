80009afd3c974d60dac6420ee6c15bbb4d1a1dcc
rhythm244 <63957603+rhythm244@users.noreply.github.com>
2021-09-11 10:42:16 +0700

    before useReducer

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
-
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  //ทำให้เวลา reload browser ทำให้  component ที่ check ว่า loggedin หรือไม่ยังคงเป็นไปแบบถูกต้อง
+  useEffect(() => {
+    const storeUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storeUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
--- README.md
+++ README.md
@@ -44,4 +44,4 @@ To run my code, navigate into a specific code snapshot folder via the `cd` comma
 
 Then run `npm install` to install all required dependencies (this will create a `/node_modules` folder).
 
-**Important:** If you're using the code for a module that requires API keys or a backend (e.g. the module about sending Http requests), you'll have to use **your backend URLs** or API keys. Mine won't work (I disabled my projects).
+**Important:** If you're using the code for a module that requires API keys or a backend (e.g. the module about sending Http requests), you'll have to use **your backend URLs** or API keys. Mine won't work (I disabled my proje
