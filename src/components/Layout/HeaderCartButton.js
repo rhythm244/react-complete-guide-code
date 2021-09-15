7c45e2195e928b335f9c66dc227c5b66777cbf2e
rhythm244 <63957603+rhythm244@users.noreply.github.com>
2021-09-15 10:42:50 +0700

    try context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,10 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
