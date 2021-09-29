93af30c0984ad221d176257320b9d3c6ebc4f988
rhythm244 <63957603+rhythm244@users.noreply.github.com>
2021-09-29 08:41:06 +0700

    complete customHook

--- src/App.js
+++ src/App.js
@@ -1,43 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useFetch from "./hook/useFetch";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
+  const url =
+    "https://react-course-udemy-f670c-default-rtdb.asia-southeast1.firebasedatabase.app/mew4479532214sdvsdCyo9hv458mevtwicouh/tasks.json";
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
+  const { sendRequest: fetchTasks, isLoading, error } = useFetch();
+  //applyData in hook call this function
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
+  useEffect(() => {
+    const tranformTasks = (taskObj) => {
       const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
       }
 
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+    }; //ไม่มี dependency เพราะ ไม่ได้ใช้อะไรที่เป็น external มีแค่ setTasks ที่ React มัน confirm ว่ามันจะไม่เปลี่ยนแปลงใน Component นี้อยู่แล้ว
 
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks({ url: url }, tranformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,43 +1,36 @@
-import { useState } from 'react';
+import { useState } from "react";
+import useFetch from "../../hook/useFetch";
 
-import Section from '../UI/Section';
-import TaskForm from './TaskForm';
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskRequest } = useFetch();
 
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+  const url =
+    "https://react-course-udemy-f670c-default-rtdb.asia-southeast1.firebasedatabase.app/mew4479532214sdvsdCyo9hv458mevtwicouh/tasks.json";
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
 
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    props.onAddTask(createdTask);
   };
 
+  const enterTaskHandler = async (taskText) => {
+    sendTaskRequest(
+      {
+        url: url,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: { text: taskText },
+      },
+      //pre config with taskText
+      createTask.bind(null, taskText)
+    );
+  };
   return (
     <Section>
       <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
