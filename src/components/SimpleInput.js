872a609e0f1bb7a6ab4abde4f8061c29a17dcabb
rhythm244 <63957603+rhythm244@users.noreply.github.com>
2021-10-12 13:52:11 +0700

    before quit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,53 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsValid = !enteredNameIsValid && enteredNameTouched;
+
+  const formSubmitssionHandler = (event) => {
+    event.preventDefault();
+    setEnteredNameTouched(true);
+
+    if (!enteredNameIsValid) {
+      return;
+    }
+
+    console.log(enteredName);
+
+    setEnteredName("");
+    setEnteredNameTouched(false);
+  };
+
+  const nameInputClasses = nameInputIsValid
+    ? "form-control invalid"
+    : "form-control ";
+
+  const nameInputChangehandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const nameInputBlur = () => {
+    setEnteredNameTouched(true);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitssionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangehandler}
+          //onBlur จะทำงานเมื่อ Input Lose focus
+          onBlur={nameInputBlur}
+          value={enteredName}
+        />
+        {nameInputIsValid && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
