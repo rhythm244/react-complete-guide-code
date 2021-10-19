2aa004e326ddca4b7e3c850457dc2733847d6b83
rhythm244 <63957603+rhythm244@users.noreply.github.com>
2021-10-20 02:46:22 +0700

    complete Form

--- src/hook/use-customHook-thong.js
+++ src/hook/use-customHook-thong.js
@@ -1,27 +1,54 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initaiInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return {
+      value: action.value,
+      isTouched: state.isTouched,
+    };
+  }
+  if (action.type === "BLUR") {
+    return { isTouched: true, value: state.value };
+  }
+  if (action.type === "RESET") {
+    return { isTouched: false, value: "" };
+  }
+
+  //init state
+  return initaiInputState;
+};
 
 const useInputThong = (validateFn) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initaiInputState
+  );
 
-  const valueIsValid = validateFn(enteredValue);
-  const hasError = !valueIsValid && isTouched;
-
-  const inputChangehandler = (e) => {
-    setEnteredValue(e.target.value);
+  const inputChangehandler = (event) => {
+    dispatch({
+      type: "INPUT",
+      value: event.target.value,
+    });
   };
 
   const inputBlurHandler = (e) => {
-    setIsTouched(true);
+    dispatch({ type: "BLUR" });
   };
 
   const reset = () => {
-    setEnteredValue("");
-    setIsTouched(false);
+    dispatch({ type: "RESET" });
   };
 
+  const valueIsValid = validateFn(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
+
   return {
-    value: enteredValue,
+    value: inputState.value,
     isValid: valueIsValid,
     hasError,
     inputChangehandler,
