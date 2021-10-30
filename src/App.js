dd375786d310bb62afc1f6fffe9859a5371604ce
rhythm244 <63957603+rhythm244@users.noreply.github.com>
2021-10-30 16:19:24 +0700

    complate http useEff before implement with thnuk

--- src/App.js
+++ src/App.js
@@ -1,15 +1,91 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
+import { uiActions } from "./components/store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisble);
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  //ใช้ useEffect ในการอัพเดท backend เมื่อ state ของ frontend เปลี่ยนแปลง
+  useEffect(() => {
+    let timer;
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending....",
+          message: "Sending cart data.",
+        })
+      );
+      const response = await fetch(
+        "https://react-course-udemy-f670c-default-rtdb.asia-southeast1.firebasedatabase.app/mew4479532214sdvsdCyo9hv458mevtwicouh/redux.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Something error sending request.");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "seccess",
+          title: "Seccess....",
+          message: "sent cart data successfully.",
+        })
+      );
+
+      timer = setTimeout(() => {
+        dispatch(uiActions.hieNotification());
+      }, 2000);
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error....",
+          message: error.message,
+        })
+      );
+    });
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cart, dispatch]);
+
+  //JSX
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- src/components/store/ui-slice.js
+++ src/components/store/ui-slice.js
@@ -2,15 +2,24 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
   name: "ui",
-  initialState: { cartIsVisble: false },
+  initialState: { cartIsVisble: false, notification: null },
   //a map of method
   reducers: {
     toggle(state) {
       state.cartIsVisble = !state.cartIsVisble;
     },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+    hieNotification(state) {
+      state.notification = null;
+    },
   },
 });
-
 
 export const uiActions = uiSlice.actions;
 
