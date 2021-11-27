276d884863bf08c3a549b392daefe35a0c40435b
rhythm244 <63957603+rhythm244@users.noreply.github.com>
2021-11-27 20:10:50 +0700

    browerlist

---
+++ .next/static/GepjW8UECPVuc3lyufE1f/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F58eb9531e31b4d18f94e.css","static\u002Fchunks\u002Fpages\u002Findex-f70cd48d48437ec547a7.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-59a31d2b6908c281a075.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
